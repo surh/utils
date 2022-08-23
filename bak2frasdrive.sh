8b7275318ae5a771d537c1bcf8fc53a2b09644bb
Sur Herrera Paredes <surh@sh03-ln08.stanford.edu>
2022-08-23 16:40:46 -0700

    Full script. Needs pre-compression & usage. Maybe chunking

--- bak2frasdrive.sh
+++ bak2frasdrive.sh
@@ -32,13 +32,31 @@ if [[ $gdrive_path == "" ]]; then
 	echo "gdrive path not provided, using $gdrive_path"
 fi
 
-
 # echo "indir is $indir"
 # echo "gdrive is $gdrive"
 # echo "gdrive_path is $gdrive_path"
 # echo "pre_compress is $pre_compress"
 
-# exit 0
+# Check rclone
+if [ ! $(type -P rclone) ]; then
+	echo "rclone executable not found"
+	exit 3
+else
+	# echo "Checking remotes"
+	remotelist=`rclone listremotes`
+	remote_found=0
+	for remote in $remotelist; do
+		if [[ $remote == "$gdrive:" ]]; then
+			remote_found=1
+			break
+		fi
+	done
+
+	if [ $remote_found == 0 ]; then
+		echo "The specified gdrive ($gdrive) was not found"
+		exit 4
+	fi
+fi
 
 # First compress all files.
 compressed=0
@@ -46,20 +64,23 @@ if [[ "$pre_compress" == "true" ]]; then
 	file_exceptions='! -name "*.gz" ! -name "*.bam" ! -name "*.bz2" ! -name "*.zip"'
 	dir_exceptions='! -path "*/.snakemake/*"'
 	echo "Compressiing files inside $indir"
-	find $indir -type f $file_exceptions $dir_exceptions -exec gzip {} \\;
+	echo ">find $indir -type f $file_exceptions $dir_exceptions -exec gzip {} \;"
+	find $indir -type f "$file_exceptions $dir_exceptions" -exec gzip {} \;
 	compressed=1
 fi
 
 # Create tar file
 name=`basename $indir`
 tar_file="$name.tar"
-dir_exceptions='--exclude=".snakemake"'
+dir_exceptions="--exclude=.snakemake"
 if [ $compressed == 1 ]; then
 	echo "Taring $indir"
-	tar $dir_exceptions -cvvf $tar_file $indir
+	echo ">tar $dir_exceptions -cvvf $tar_file $indir"
+	tar "$dir_exceptions" -cvvf $tar_file $indir
 elif [ $compressed == 0 ]; then
 	tar_file="$tar_file.gz"
 	echo "Taring and compressing $indir"
+	echo ">tar $dir_exceptions -cvvzf $tar_file $indir"
 	tar $dir_exceptions -cvvzf $tar_file $indir
 else
 	echo "Unexpected compressed flag ($compressed)"
@@ -67,6 +88,8 @@ else
 fi
 
 
-# Rclone to fraserdrive
-# rclone sync --tpslimit 2 --transfers 1 $tar_file $gdrive:$gdrive_path --verbose
+# Rclone to gdrive
+echo "Uploading to gdrive with rclone"
+echo ">rclone sync --tpslimit 2 --transfers 1 $tar_file $gdrive:$gdrive_path --verbose"
+rclone sync --tpslimit 2 --transfers 1 $tar_file $gdrive:$gdrive_path --verbose
 
