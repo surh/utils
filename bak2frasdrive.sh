588b97e41bbfd3cafd3cc8c58a96fcc40dedf005
Sur Herrera Paredes <surh@sh03-ln08.stanford.edu>
2022-08-23 15:47:19 -0700

    Script tars with or without compression

--- bak2frasdrive.sh
+++ bak2frasdrive.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/env bash
 
-indir=$1
-gdrive="fraserlab_gdrive"
-gdrive_path=""
+# Read options
+pre_compress=false
+while getopts d:g:p:b flag
+do
+	case "${flag}" in
+		d) indir=${OPTARG};;
+		g) gdrive=${OPTARG};;
+		p) gdrive_path=${OPTARG};;
+		b) pre_compress=true;;
+	esac
+done
+# indir=$1
+# gdrive="fraserlab_gdrive"
+# gdrive_path=""
 
+# Set parameters defaults
 if [[ $indir == "" ]]; then
 	echo "No input directory specified"
 	exit 1
+fi
+
+if [[ $gdrive == "" ]]; then
+	gdrive="fraserlab_gdrive"
+	echo "No rclone remote specified. Trying $gdrive"
 fi
 
 if [[ $gdrive_path == "" ]]; then
@@ -15,19 +32,41 @@ if [[ $gdrive_path == "" ]]; then
 	echo "gdrive path not provided, using $gdrive_path"
 fi
 
-# First compress all files.
-# Exceptions for allready compressed files & bam files
-# When untaring and uncompressing, this will break links to compressed files
-find $indir -type f ! -name "*.gz" ! -name "*.bam" ! -name "*.bz2" ! -name "*.zip" ! -path "*/.snakemake/*" -exec gzip {} \;
 
-# Now create a tar file of compressed files
+# echo "indir is $indir"
+# echo "gdrive is $gdrive"
+# echo "gdrive_path is $gdrive_path"
+# echo "pre_compress is $pre_compress"
+
+# exit 0
+
+# First compress all files.
+compressed=0
+if [[ "$pre_compress" == "true" ]]; then
+	file_exceptions='! -name "*.gz" ! -name "*.bam" ! -name "*.bz2" ! -name "*.zip"'
+	dir_exceptions='! -path "*/.snakemake/*"'
+	echo "Compressiing files inside $indir"
+	find $indir -type f $file_exceptions $dir_exceptions -exec gzip {} \\;
+	compressed=1
+fi
+
+# Create tar file
 name=`basename $indir`
 tar_file="$name.tar"
-# echo $tar_file
-#tar -cvvf $tar_file $indir
-tar --exclude=".snakemake" -cvvf $tar_file $indir
+dir_exceptions='--exclude=".snakemake"'
+if [ $compressed == 1 ]; then
+	echo "Taring $indir"
+	tar $dir_exceptions -cvvf $tar_file $indir
+elif [ $compressed == 0 ]; then
+	tar_file="$tar_file.gz"
+	echo "Taring and compressing $indir"
+	tar $dir_exceptions -cvvzf $tar_file $indir
+else
+	echo "Unexpected compressed flag ($compressed)"
+	exit 2
+fi
+
 
 # Rclone to fraserdrive
-rclone sync --tpslimit 2 --transfers 1 $tar_file $gdrive:$gdrive_path --verbose
-
+# rclone sync --tpslimit 2 --transfers 1 $tar_file $gdrive:$gdrive_path --verbose
 
